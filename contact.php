ebeeb6c62ac804b158d3d13dfa5463d34f68fdb7
eliasdengo <eliasdengo5@gmail.com>
2023-03-17 01:07:13 +0300

    send email from static page try

--- contact.php
+++ contact.php
@@ -19,3 +19,10 @@ if(isset($_POST['sendmessage'])){
      header('Location: https://eliasdengo.github.io/'); 
     }
 ?>
+
+<form action="http://formspree.io/you@email.com">
+<input type="email" name="_replyto">
+<textarea name="body">
+
+</textarea><input type="submit" value="Send">
+</form>
